bd4b5062b51946999ce6c85cc9b572673e429df4
Nilavodas <nilavodas57@gmail.com>
2025-05-19 20:05:13 +0530

    updated APIS today

---
+++ src/main/java/com/example/mayPackage/Today.java
@@ -0,0 +1,15 @@
+package com.example.mayPackage;
+
+public class Today {
+
+    private String value;
+    private String secondValue;
+
+    public String getSecondValue() {
+        return secondValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
